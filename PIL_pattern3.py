c51d1e59a968025951a8c44a3e2c8820895d36d0
Siddharth <nairsiddu@gmail.com>
2024-01-07 18:11:57 +0000

    Continuing work on ImageChops and Deformer

---
+++ PIL_pattern3.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageOps
+
+class Deformer():
+    def getmesh(self, img):
+        w,h = img.size
+        # define a shape in the original image
+        # source_shape = (0,0,0,h,w,h,w,0 ) 
+        # define a rectangle in the new image
+        # target_rect = (0,0,w,h) # top left, bottom right
+
+        topleft = ((w // 2, 0, w, h // 2),(0, 0, 0, h, w // 2, h, w // 2, 0))
+        topright = ((0, 0, w // 2, h // 2),(w // 2, 0, w // 2, h, 0, h, 0, 0))
+        bottomleft = ((w // 2, h // 2, w, h),(0, h, 0, 0, w // 2, 0, w // 2, h))
+        bottomright = ((0, h // 2, w // 2, h),(w // 2, h, w // 2, 0, 0, 0, 0, h))
+
+        # return all the shapes
+        return [topleft, topright, bottomleft, bottomright]
+# image import
+image = Image.open('img1.jpg')
+deform = ImageOps.deform(image, Deformer())
+deform.show()
+
