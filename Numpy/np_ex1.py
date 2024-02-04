1ebb316d10fbc11bab3eb6f00baab321c138e2e8
Siddharth <nairsiddu@gmail.com>
2024-02-04 20:37:11 +0000

    further changes

--- Numpy/np_ex1.py
+++ Numpy/np_ex1.py
@@ -4,11 +4,11 @@ a = np.ones((5,5))
 b = np.zeros((3,3))
 
 b[1,1] = 9
-a[1:4, 1:4] = b
+a[1:4, 1:4] = b 
 
 print(a)
 
-
+# startindex:endindex:stepsize
 
 
 
--- project1.py
+++ project1.py
@@ -5,11 +5,11 @@ from PIL import Image, ImageFilter, ImageEnhance
 aud = wave.open("triumph.wav")
 
 sample_freq = aud.getframerate()
-frames = aud.readframes(aud.getnframes())
+frames = aud.readframes(aud.getnframes()) # reads all the frames of the audio
 audio_wave = aud.readframes(-1)
-time = frames / sample_freq
+sample_width = aud.getsampwidth() # time = frames / sample_freq
 
-audio_array = np.frombuffer(audio_wave, dtype=np.int16)
+audio_array = np.frombuffer(audio_wave, dtype=np.int16) # Interprets the number of frames as a 1-dimentional array
 aud.close()
 
 image = Image.open('img1.jpg')
@@ -25,12 +25,11 @@ sharpness_enhancer = ImageEnhance.Sharpness(image)
 # cont_value = print(sum(abs(int.from_bytes((audio_array[25000]) / 10000), byteorder='little', signed=True)))
 print(len(audio_array))
 
-cont_value = sum(abs(int.from_bytes(frames[i:i+time], byteorder='little', signed=True))
-                         for i in range(0, len(frames), time)) / (len(frames) / 50)
+cont_value = sum(abs(int.from_bytes(frames[i:i+sample_width], byteorder='little', signed=True)) 
+                         for i in range(0, len(frames), sample_width)) / len(frames) # finding the mean amplitude
 
 image2 = contrast_enhancer.enhance(cont_value)
 image2.show
-
 
 
 
